945e19937d49a1de67d584ca0a91ff921f6f6863
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2019-03-28 11:24:47 -0400

    Use magisk tools now

---
+++ common/boot_patch.sh
@@ -0,0 +1,173 @@
+#!/system/bin/sh
+##########################################################################################
+#
+# Magisk Boot Image Patcher
+# by topjohnwu
+#
+# Usage: boot_patch.sh <bootimage>
+#
+# The following flags can be set in environment variables:
+# KEEPVERITY, KEEPFORCEENCRYPT
+#
+# This script should be placed in a directory with the following files:
+#
+# File name          Type      Description
+#
+# boot_patch.sh      script    A script to patch boot. Expect path to boot image as parameter.
+#                  (this file) The script will use binaries and files in its same directory
+#                              to complete the patching process
+# util_functions.sh  script    A script which hosts all functions requires for this script
+#                              to work properly
+# magiskinit         binary    The binary to replace /init, which has the magisk binary embedded
+# magiskboot         binary    A tool to unpack boot image, decompress ramdisk, extract ramdisk,
+#                              and patch the ramdisk for Magisk support
+# chromeos           folder    This folder should store all the utilities and keys to sign
+#                  (optional)  a chromeos device. Used for Pixel C
+#
+# If the script is not running as root, then the input boot image should be a stock image
+# or have a backup included in ramdisk internally, since we cannot access the stock boot
+# image placed under /data we've created when previously installed
+#
+##########################################################################################
+##########################################################################################
+# Functions
+##########################################################################################
+
+# Pure bash dirname implementation
+getdir() {
+  case "$1" in
+    */*) dir=${1%/*}; [ -z $dir ] && echo "/" || echo $dir ;;
+    *) echo "." ;;
+  esac
+}
+
+keytest() {
+  ui_print "- Vol Key Test"
+  ui_print "   Press a Vol Key:"
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+  return 0
+}
+
+chooseport() {
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while true; do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 0
+  else
+    return 1
+  fi
+}
+
+chooseportold() {
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  $INSTALLER/$ARCH32/keycheck
+  $INSTALLER/$ARCH32/keycheck
+  SEL=$?
+  if [ "$1" == "UP" ]; then
+    UP=$SEL
+  elif [ "$1" == "DOWN" ]; then
+    DOWN=$SEL
+  elif [ $SEL -eq $UP ]; then
+    return 0
+  elif [ $SEL -eq $DOWN ]; then
+    return 1
+  else
+    abort "   Vol key not detected!"
+  fi
+}
+
+# patch_cmdline <cmdline entry name> <replacement string>
+patch_cmdline() {
+  local cmdline match;
+  cmdline=`grep 'cmdline=' header`
+  if [ -z "$(echo $cmdline | grep "$1")" ]; then
+    [ -z $2 ] && return || cmdline="$cmdline $2"
+  else
+    [ -z $2 ] && cmdline="$(echo $cmdline | sed -e "s|$1 ||" -e "s| $1||")" || cmdline="$(echo $cmdline | sed -e "s|$1|$2|")"
+  fi
+  sed -i "s|cmdline=.*|$cmdline|" header
+}
+
+##########################################################################################
+# Initialization
+##########################################################################################
+
+if [ -z $SOURCEDMODE ]; then
+  # Switch to the location of the script file
+  cd "`getdir "${BASH_SOURCE:-$0}"`"
+  # Load utility functions
+  . ./util_functions.sh
+fi
+
+BOOTIMAGE="$1"
+[ -e "$BOOTIMAGE" ] || abort "$BOOTIMAGE does not exist!"
+
+chmod -R 755 .
+
+# Extract magisk if doesn't exist
+[ -e magisk ] || ./magiskinit -x magisk $MAGISKBIN/magisk
+
+##########################################################################################
+# Unpack
+##########################################################################################
+
+CHROMEOS=false
+
+ui_print "- Unpacking boot image"
+./magiskboot unpack -h "$BOOTIMAGE"
+
+case $? in
+  1 )
+    abort "! Unsupported/Unknown image format"
+    ;;
+  2 )
+    ui_print "- ChromeOS boot image detected"
+    CHROMEOS=true
+    ;;
+esac
+
+##########################################################################################
+# Ramdisk patches
+##########################################################################################
+
+# begin ramdisk changes
+if keytest; then
+  FUNCTION=chooseport
+else
+  FUNCTION=chooseportold
+  ui_print "   ! Legacy device detected! Using old keycheck method"
+  ui_print " "
+  ui_print "- Vol Key Programming"
+  ui_print "   Press Vol Up Again:"
+  $FUNCTION "UP"
+  ui_print "   Press Vol Down"
+  $FUNCTION "DOWN"
+fi
+ui_print " "
+ui_print "- Select Sepolicy"
+ui_print "   Vol+ = Enforcing, Vol- = Permissive"
+if $FUNCTION; then
+  ui_print "   Setting kernel to enforcing..."
+  patch_cmdline "androidboot.selinux=permissive" ""
+else
+  ui_print "   Setting kernel to permissive..."
+  patch_cmdline "androidboot.selinux=permissive" "androidboot.selinux=permissive"
+fi
+
+##########################################################################################
+# Repack and flash
+##########################################################################################
+
+ui_print "- Repacking boot image"
+./magiskboot repack "$BOOTIMAGE" || abort "! Unable to repack boot image!"
+
+# Sign chromeos boot
+$CHROMEOS && sign_chromeos
+
+# Reset any error code
+true
